bb906058f56413c32b3a07738f4b48f6acd23d83
kumar <kumarnsanthosh1998@gmail.com>
2026-03-10 17:47:27 +0530

    added first course and details

--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Instructor(models.Model):
     name = models.CharField(max_length=30)
-    image = models.ImageField(upload_to='images/instructor_images')
+    image = models.ImageField(upload_to='static/images/instructor_images')
     description = models.TextField()
     def __str__(self):
         return self.name
@@ -17,7 +17,8 @@ class Course(models.Model):
         return self.name
 
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/course_images')
+    image = models.ImageField(upload_to='static/images/course_images')
+    video = models.CharField(max_length=500, default='video-link')
     instructor  = models.ForeignKey(Instructor, on_delete=models.CASCADE)
     description = models.TextField()
     rating = models.FloatField()
