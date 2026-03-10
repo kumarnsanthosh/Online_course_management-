115a08a2b7875a620981a185bf5e19cead17e33b
kumar <kumarnsanthosh1998@gmail.com>
2026-03-10 15:39:35 +0530

    created model instructor and course

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Instructor(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/instructor_images')
+    about = models.CharField(max_length=20)
+    description = models.TextField()
+
+
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/course_images')
+    instructor  = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    description = models.TextField()
+    rating = models.FloatField()
+    price = models.FloatField()
+    hours = models.FloatField()
+
+
